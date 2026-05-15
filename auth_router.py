76572db77faff17e04fcbedf54285a73853eb5a8
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-05-15 15:45:22 +0100

    update

--- auth_router.py
+++ auth_router.py
@@ -1,11 +1,34 @@
-from fastapi import APIRouter
+from pydantic import EmailStr
+from fastapi import APIRouter, HTTPException, Depends
 from models import SessionDep, User, UserBase
-from passlib.context import CryptContext
-
-becrypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from jose import jwt
+from main import SECRET_KEY,ALGORITHM, becrypt
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 auth_router = APIRouter(tags=["auth"],prefix="/auth")
+
+
+
+def creat_token(id, duration_token =timedelta(minutes=30)):
+    date_expiretaion = datetime.now(timezone.utc) + duration_token
+    dict_info = {"sub": str(id), "exp":int(date_expiretaion.timestamp())}
+    jwt_token = jwt.encode(dict_info, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt_token
+
+
+
+def user_autenticate(email,password,session:SessionDep):
+    user = session.get(User,email)
+    if not user:
+        return False
+    elif not becrypt.verify(password,user.password):
+        return False
+    else:
+        return user
+
+
 
 @auth_router.post("/Creat_account",response_model=User)
 async def Creat_account(user: UserBase, session: SessionDep):
@@ -21,6 +44,14 @@ async def Creat_account(user: UserBase, session: SessionDep):
 
     return user_form
 
-@auth_router.post("/login")
-async def login():
-    ...
+@auth_router.post("/login-form")
+async def login_form(session: SessionDep, form_data:OAuth2PasswordRequestForm = Depends()):
+    user = user_autenticate(form_data.password,form_data.username,session)
+    if not user:
+        raise HTTPException(status_code=404, detail="User does not exist or invalid credentials")
+    else:
+        access_token = creat_token(user.id)
+        return {
+            "access_token":access_token,
+            "token_type":"bearer",
+        }
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import os
 from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from auth_router import auth_router
 from models import create_db_and_tables
+from dotenv import load_dotenv
+from passlib.context import CryptContext
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 
 app = FastAPI()
 
+Oauth_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-form")
+becrypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-Oauth_scheme = OAuth2PasswordBearer(tokenUrl="auth")
 
+
+from auth_router import auth_router
 
 app.include_router(auth_router)
 
