9f54a89bc896a71abfebfcc87431fea8c7a1f63e
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-05-16 06:38:16 +0100

    update

--- auth_router.py
+++ auth_router.py
@@ -1,8 +1,8 @@
-from pydantic import EmailStr
+from sqlmodel import select
 from fastapi import APIRouter, HTTPException, Depends
 from models import SessionDep, User, UserBase
 from jose import jwt
-from main import SECRET_KEY,ALGORITHM, becrypt
+from main import SECRET_KEY,ALGORITHM, becrypt, Oauth_scheme
 from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -17,16 +17,26 @@ def creat_token(id, duration_token =timedelta(minutes=30)):
     jwt_token = jwt.encode(dict_info, SECRET_KEY, algorithm=ALGORITHM)
     return jwt_token
 
+def get_current_user(session:SessionDep,token: str = Depends(Oauth_scheme)):
+    verify = jwt.decode(token,SECRET_KEY,ALGORITHM)
+
+    user_id = verify.get("sub")
+
+    user = session.get(User,user_id)
+
+    return user
 
 
-def user_autenticate(email,password,session:SessionDep):
-    user = session.get(User,email)
-    if not user:
+
+def user_autenticate(email,password, session:SessionDep):
+    user = select(User).where(User.email == email)
+    users = session.exec(user).first()
+    if not users:
         return False
-    elif not becrypt.verify(password,user.password):
+    elif not becrypt.verify(password,users.password):
         return False
-    else:
-        return user
+
+    return users
 
 
 
@@ -45,13 +55,18 @@ async def Creat_account(user: UserBase, session: SessionDep):
     return user_form
 
 @auth_router.post("/login-form")
-async def login_form(session: SessionDep, form_data:OAuth2PasswordRequestForm = Depends()):
-    user = user_autenticate(form_data.password,form_data.username,session)
+async def login_form(
+        session: SessionDep,
+        form_data:OAuth2PasswordRequestForm = Depends()):
+
+    user = user_autenticate(form_data.username,form_data.password,session)
     if not user:
-        raise HTTPException(status_code=404, detail="User does not exist or invalid credentials")
-    else:
-        access_token = creat_token(user.id)
-        return {
+        print("não esta a achar o objeto")
+        raise HTTPException(status_code=404, detail="não esta a achar o objeto")
+
+    access_token = creat_token(user.id)
+    return {
             "access_token":access_token,
             "token_type":"bearer",
         }
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import os
-from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from models import create_db_and_tables
+from fastapi import FastAPI, Depends
+from fastapi.security import  OAuth2PasswordBearer
+from models import create_db_and_tables, User
 from dotenv import load_dotenv
 from passlib.context import CryptContext
+import os
 
 load_dotenv()
 
@@ -13,13 +13,14 @@ ALGORITHM = os.getenv("ALGORITHM")
 
 app = FastAPI()
 
-Oauth_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-form")
+
+
 becrypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+Oauth_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-form")
 
 
-
-from auth_router import auth_router
+from auth_router import auth_router, get_current_user
 
 app.include_router(auth_router)
 
@@ -27,6 +28,9 @@ app.include_router(auth_router)
 async def startup():
     create_db_and_tables()
 
+@app.get("/me")
+async def readme(user:User = Depends(get_current_user)):
+    return user
 
 
 
